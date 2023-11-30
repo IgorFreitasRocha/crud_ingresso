377b38212cf797f52bac2d1f4611e89f2d861b02
MauMau221 <mauriciobgsp@gmail.com>
2023-11-30 16:17:32 -0300

    JS do botão de ativo e inativo em listar produtos

--- CRUD/js/javinha.js
+++ CRUD/js/javinha.js
@@ -1,5 +1,26 @@
+//Função para gerar mensagem de confirmação ao cadastrar/atualizar
 let contentdiv = document.getElementById('messagee');
       setTimeout(() => {
          contentdiv.style.display = 'none';
       },3500);
 
+
+
+
+//Botão de ativo e intativo em exibir produtos
+   let active = true;
+
+  function toggleStatus() {
+    const activeButton = document.querySelector('.btn-primary');
+    const inactiveButton = document.querySelector('.btn-danger');
+
+    if (active) {
+      activeButton.classList.remove('active');
+      inactiveButton.classList.add('active');
+      active = false;
+    } else {
+      activeButton.classList.add('active');
+      inactiveButton.classList.remove('active');
+      active = true;
+    }
+  }
